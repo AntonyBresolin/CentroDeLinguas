522aa089c2018aaee7129cf895e62bb9741d1e40
Antony Henrique Bresolin <40371352+AntonyBresolin@users.noreply.github.com>
2023-05-02 21:42:44 -0300

    Adição das elipses, quadrados e outras arquitetura

---
+++ main.js
@@ -0,0 +1,10 @@
+const header = document.querySelector("header");
+const imagem_logo = document.querySelector("#logo");
+
+
+window.addEventListener("scroll", function () {
+  header.classList.toggle("sticky", window.scrollY > 20);
+});
+window.addEventListener("scroll", function () {
+    imagem_logo.classList.toggle("Nlogo", window.scrollY > 20);
+  });
