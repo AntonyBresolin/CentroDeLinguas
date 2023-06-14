ba97b3e1359103e8783092f9086e4b5c51d8e9c0
Antony Henrique Bresolin <40371352+AntonyBresolin@users.noreply.github.com>
2023-06-14 16:35:38 -0300

    Atualização geral
    - Adicionando menu em todas as pages
    - Correção de bugs no codigo
    - Pequenas alterações no quizz, transform, identação da pagina
    - Retirando popup de notificação de todas pages
    - Retirado index.js
    - retirado nodemodules
    - organização do main js, fracionando codigo e concertado a nomenclatura.
    - Remoção de imagens não utilizadas.
    - Correção de estrutura do codigo css, deixando alinhado em conformidade com a utilização.

---
+++ src/js/navbar.js
@@ -0,0 +1,57 @@
+const header = document.querySelector(".header");
+const imagem_logo = document.querySelector("#logo");
+const background_celular = document.querySelector(".backgroud-menu");
+const midias_sociais = document.querySelector(".midias-celular");
+
+
+class MobileNavbar {
+    constructor(mobileMenu, navList, navLinks) {
+      this.mobileMenu = document.querySelector(mobileMenu);
+      this.navList = document.querySelector(navList);
+      this.navLinks = document.querySelectorAll(navLinks);
+      this.activeClass = "active";
+      
+  
+      this.handleClick = this.handleClick.bind(this);
+    }
+  
+    animateLinks() {
+      this.navLinks.forEach((link, index) => {
+        link.style.animation
+          ? (link.style.animation = "")
+          : (link.style.animation = `navLinkFade 1s ease forwards ${
+              index / 7 + 0.3
+            }s`);
+      });
+    }
+  
+    handleClick() {
+      this.navList.classList.toggle(this.activeClass);
+      this.mobileMenu.classList.toggle(this.activeClass);
+      background_celular.classList.toggle("background-celular-ativo");
+      midias_sociais.classList.toggle("midias-celular-ativo");
+      header.classList.toggle("header-celular-ativo");
+      
+      this.animateLinks();
+    }
+  
+    addClickEvent() {
+      this.mobileMenu.addEventListener("click", this.handleClick);
+    }
+  
+    init() {
+      if (this.mobileMenu) {
+        this.addClickEvent();
+      }
+      return this;
+    }
+  }
+  
+  const mobileNavbar = new MobileNavbar(
+    ".mobile-menu",
+    ".nav-list",
+    ".nav-list li",
+  );
+  mobileNavbar.init();
+
+
